6605536449213c64b26c0a40db0ede2b6ad0cfaa
Michael Siegfried <michael@procore.com>
2015-05-30 16:02:30 -0700

    Adding mousetrap.g

--- app/controllers/actions_controller.rb
+++ app/controllers/actions_controller.rb
@@ -1,5 +1,32 @@
 class ActionsController < ApplicationController
   def index
   end
+
+  def create
+    activity = current_user.activities.find(params[:activity_id])
+    action = current_user.actions.new(activity: activity, start_at: Time.now)
+    if can_switch_action?(activity: activity, action: action)
+      head :ok
+    else
+      render status: :bad_request
+    end
+  end
+
+  def end
+    if current_action.end
+      head :ok
+    else
+      render status: :bad_request
+    end
+  end
+
+  private
+  def can_switch_action?(action:, activity:)
+    if current_action
+      current_action.activity != activity && current_action.end && action.save
+    else
+      action.save
+    end
+  end
 end
 
--- app/models/action.rb
+++ app/models/action.rb
@@ -2,6 +2,14 @@ class Action < ActiveRecord::Base
   belongs_to :user
   belongs_to :activity
 
-  scope :ordered, -> { order(start_at: :asc) }
+  scope :ordered, -> { order(start_at: :desc) }
+  
+  def name
+    activity.name
+  end
+
+  def end
+    update(end_at: Time.now)
+  end
 end
 
