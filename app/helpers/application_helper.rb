26ecffdd7fbad2007d42795ca3967397c597a3a6
Michael Siegfried <michael@procore.com>
2015-06-01 22:03:12 -0700

    The commit :(

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def icon_for(icon_name)
+    Icon.new(icon_name: icon_name).render
+  end
+
+  def badge_for(value:)
+    content_tag(:span, value, class: 'badge')
+  end
 end
