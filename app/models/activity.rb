bf446bbdf4e7688b299c9e0a013f340490707d26
Michael Siegfried <michael@procore.com>
2015-09-13 15:19:41 -0700

    Remove other references to keybind

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -2,7 +2,7 @@ class Activity < ActiveRecord::Base
   belongs_to :user
   has_many :actions
 
-  validates :key_bind, inclusion: { in: 'a'.upto('z').to_a }, presence: true, uniqueness: { scope: :user_id }
+  validates :name, presence: true, uniqueness: { scope: :user_id }
 
   def total_length_of_time
     actions.completed.map(&:length).sum
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150526041236) do
+ActiveRecord::Schema.define(version: 20150913221733) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,7 +29,6 @@ ActiveRecord::Schema.define(version: 20150526041236) do
   create_table "activities", force: :cascade do |t|
     t.integer "user_id"
     t.text    "name"
-    t.text    "key_bind"
   end
 
   add_index "activities", ["user_id"], name: "index_activities_on_user_id", using: :btree
