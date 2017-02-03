cf26beff500d5d439d2c3a7a845b1b0fbe56038c
Michael Siegfried <michael@procore.com>
2017-02-02 17:28:54 -0800

    Remove validations for removed columns.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,5 @@
 class User < ActiveRecord::Base
-  validates :username, presence: true, uniqueness: true, length: { in: 3..20 }
   validates :email, presence: true, uniqueness: true, format: /@/
-  validates :password, length: { in: 6..72 }, confirmation: true
 
   has_many :actions
   has_many :activities
