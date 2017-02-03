62601d5c033c8a2f798122cee539758fb3cd6d70
Michael Siegfried <michael@procore.com>
2017-02-02 17:29:35 -0800

    Destroy dependent items upon deletion.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   validates :email, presence: true, uniqueness: true, format: /@/
 
-  has_many :actions
-  has_many :activities
+  has_many :actions, dependent: :destroy
+  has_many :activities, dependent: :destroy
 end
